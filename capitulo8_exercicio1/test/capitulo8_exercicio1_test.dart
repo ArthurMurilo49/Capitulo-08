9a533d06ad398ab45e57874aad19e15ca9520c4d
Arthur Murilo Santos da Cunha <arthurmurilo49@gmail.com>
2023-02-01 18:24:58 -0300

    Exercicio calculo do IMC e o Exercicio do Capitulo 2

---
+++ capitulo8_exercicio1/test/capitulo8_exercicio1_test.dart
@@ -0,0 +1,8 @@
+import 'package:capitulo8_exercicio1/capitulo8_exercicio1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ capitulo8_exercicio2/test/capitulo8_exercicio2_test.dart
@@ -0,0 +1,8 @@
+import 'package:capitulo8_exercicio2/capitulo8_exercicio2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
